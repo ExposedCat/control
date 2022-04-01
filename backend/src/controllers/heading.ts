bad0c3ae9bf09113035cb62c0267f7c768740ba5
Artem Prokop <artem13.prokop@gmail.com>
2022-04-01 21:48:13 +0200

    ➖ Remove dummy controllers

--- backend/src/controllers/heading.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-
-function headingController(request: Request, response: Response) {
-	response.json({
-		heading: `Home`
-	})
-}
-
-export { headingController }
